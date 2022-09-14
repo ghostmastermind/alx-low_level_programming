318bad6cc3b8d9f7444622572f9f74683945f9cf
ghostmastermind <ekenekingsleynwajiaku@gmail.com>
2022-09-14 08:54:34 +0000

    C program that prints exactly

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,17 +1,17 @@
-nclude <stdio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
  * Description:
  * 'The program prints a sentence'
- *  Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but that outcome is a piece of art,\n");
 
-	return (0);
+return (0);
 
 
-
+}
