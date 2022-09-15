76e30aa5552066e3f64ce961659ad1f76db9893b
ghostmastermind <ekenekingsleynwajiaku@gmail.com>
2022-09-15 08:04:15 +0000

    program that prints _putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * maiin - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char put[8]  = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+	_putchar(put[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
