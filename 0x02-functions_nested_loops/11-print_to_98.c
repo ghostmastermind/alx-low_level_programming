4b6d4988e389620cb0121a80914795881ef8f8a9
ghostmastermind <ekenekingsleynwajiaku@gmail.com>
2022-09-16 14:13:35 +0000

    prints all natural numbers from

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Check description
+ * @n: input integer
+ * Description: It prints the alphabet in lowercase fallowed by a new line
+ * Return: Nothing.
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+	
+		for (; n <= 98; n++)
+		{
+			printf("%d", n);
+
+			if (n == 98)
+				continue;
+			printf(", ");
+		}
+		printf("\n");
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
+
+			if (n == 98)
+				continue;
+			printf(", ");
+		}
+		printf("\n");
+	}
+}
+	
